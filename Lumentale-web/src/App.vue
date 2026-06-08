09a76fd07040cc81a3f3536a144dd0d58b46589d
wanghuan <wanghuan072@gmail.com>
2026-06-08 14:55:38 +0800

    优化-2

--- Lumentale-web/src/App.vue
+++ Lumentale-web/src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <AppHeader />
-  <RouterView />
-  <AppFooter />
+  <RouterView v-slot="{ Component }">
+    <component :is="Component" v-if="Component" @vue:mounted="onRouteViewMounted" />
+  </RouterView>
+  <AppFooter v-if="footerVisible" />
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { RouterView } from 'vue-router'
 import AppHeader from '@/components/AppHeader.vue'
 import AppFooter from '@/components/AppFooter.vue'
+
+const footerVisible = ref(false)
+
+function onRouteViewMounted() {
+  footerVisible.value = true
+}
 </script>
